dc539b3543aaa491899966115db532f3c0ab2e8f
Britt Gresham <britt@brittg.com>
2023-02-24 11:05:45 -0800

    Add aws vars

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "waypoint_project" {
   type = string
   description = "Waypoint Project Name"
 }
+
+variable "AWS_ACCESS_KEY_ID" { }
+variable "AWS_SECRET_ACCESS_KEY" {
+  sensitive = true
+}
